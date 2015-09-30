df9fcd9adfc1c120e6698acc969e22995d857d9d
Kevin McIntosh <kevjames3@gmail.com>
2015-09-29 23:30:12 -0700

    Added a way to print trees and a basic add method that is similar to a BST

--- RedBlackTree.js
+++ RedBlackTree.js
@@ -1,18 +1,26 @@
+var chalk = require('chalk');
+
 exports.RedBlackTree = function (){
 	//Init
 	var self = this;
 	var rootNode;
 
 	//Private
-	function CreateNewNode(value){
+	function Node(value){
 		var self = this;
 
+		self.ValidColors = {
+			Red: "red",
+			Black: "grey"
+		};
+
+		self.Color = self.ValidColors.Red;
 		self.Value = value;
 		self.Parent = undefined;
 		self.Left = undefined;
 		self.Rgiht = undefined;
 
-		return new self;
+		return self;
 	}
 
 	function RotateLeft(){
@@ -30,7 +38,8 @@ exports.RedBlackTree = function (){
 	//Public Methods
 	self.Add = function(item){
 		if(rootNode == undefined){
-			rootNode = CreateNewNode(item);
+			rootNode = new Node(item);
+			rootNode.Color = rootNode.ValidColors.Black;
 		} else {
 			var currentNode = rootNode;
 			var nextNode = undefined;
@@ -38,13 +47,13 @@ exports.RedBlackTree = function (){
 				if (Compare(item, currentNode.Value) <= 0){
 					nextNode = currentNode.Left;
 					if(nextNode == undefined){
-						currentNode.Left = CreateNewNode(item);
+						currentNode.Left = new Node(item);
 						break;
 					}
 				} else {
 					nextNode = currentNode.Right;
 					if(nextNode == undefined){
-						currentNode.Right = CreateNewNode(item);
+						currentNode.Right = new Node(item);
 						break;
 					}
 				}
@@ -61,9 +70,66 @@ exports.RedBlackTree = function (){
 
 	}
 
+	self.PrintTree = function(){
+		var PrintTreeHelper = function(nodes, depth){
+			if(nodes.filter(function(value){ return value != undefined }).length == 0){
+				return;
+			}
+
+			var queue = [];
+			var lineQueue = [];
+			while(nodes.length > 0){
+				var node = nodes.shift();
+				
+				if(node){
+					queue.push(node.Left);
+					queue.push(node.Right);
+
+					if(node.Left){
+						lineQueue.push("|");
+					} else {
+						lineQueue.push("");
+					}
+
+					if(node.Right){
+						lineQueue.push("\\");
+					} else {
+						lineQueue.push("");
+					}
+
+					process.stdout.write(chalk[node.Color](node.Value) + Array(depth + 1).join(' '));
+				} else {
+					process.stdout.write(Array(depth + 1).join(''));
+					lineQueue.push("");
+				}
+			}
+			console.log("");
+			
+			lineQueue.forEach(function(item){
+				process.stdout.write(item + Array(depth + 1).join(' '));
+			});
+			console.log("");
+
+			PrintTreeHelper(queue, depth + 1);
+		}
+
+		var queue = [];
+		var depth = 0;
+
+		queue.push(rootNode);
+		PrintTreeHelper(queue, depth);		
+	}
+
 	return self;
 }
 
 if (require.main === module) {
-
+	var foo = new exports.RedBlackTree();
+	foo.Add(1);
+	foo.Add(2);
+	foo.Add(3);
+	foo.Add(-1);
+	foo.Add(-2);
+	foo.Add(0);
+	foo.PrintTree();
 }
